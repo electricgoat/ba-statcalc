c34d9db8a10220cfef6d4359abe2af9177ce8e3f
electricgoat <42342869+electricgoat@users.noreply.github.com>
2025-08-31 16:00:03 +0700

    Updated character table filter
    textfield filtering added

--- table_filter/charactertable.js
+++ table_filter/charactertable.js
@@ -1,77 +1,374 @@
-var charactertable_filter = [];
-$( document ).ready(function() {
+// Internal filter array
+let charactertable_filters = [];
+let quick_filter = {}; // Groups that have quick filter buttons
+let lastSearchSegments = [];
+
+// Map keywords to button groups/values
+const charactertable_search_keywords = {
+    'gehenna': { group: 'school', value: 'gehenna' },
+    'trinity': { group: 'school', value: 'trinity' },
+    'valkyrie': { group: 'school', value: 'valkyrie' },
+    'highlander': { group: 'school', value: 'highlander' },
+    'wildhunt': { group: 'school', value: 'wildhunt' },
+    'abydos': { group: 'school', value: 'abydos' },
+    'arius': { group: 'school', value: 'arius' },
+    'millennium': { group: 'school', value: 'millennium' },
+    'red_winter': { group: 'school', value: 'red_winter' },
+    'srt': { group: 'school', value: 'srt' },
+    'etc': { group: 'school', value: 'etc' },
+
+    'ar': { group: 'weapon', value: 'ar' },
+    'ft': { group: 'weapon', value: 'ft' },
+    'gl': { group: 'weapon', value: 'gl' },
+    'hg': { group: 'weapon', value: 'hg' },
+    'mg': { group: 'weapon', value: 'mg' },
+    'rg': { group: 'weapon', value: 'rg' },
+    'rl': { group: 'weapon', value: 'rl' },
+    'sg': { group: 'weapon', value: 'sg' },
+    'smg': { group: 'weapon', value: 'smg' },
+    'sr': { group: 'weapon', value: 'sr' },
+
+    'front': { group: 'position', value: 'front' },
+    'middle': { group: 'position', value: 'middle' },
+    'back': { group: 'position', value: 'back' },
+
+    'attacker': { group: 'role', value: 'attacker' },
+    'tank': { group: 'role', value: 'tank' },
+    'healer': { group: 'role', value: 'healer' },
+    'support': { group: 'role', value: 'support' },
+    'tacticalsupport': { group: 'role', value: 'ts' },
+    'ts': { group: 'role', value: 'ts' },
+    't.s': { group: 'role', value: 'ts' },
+
+    'striker': { group: 'class', value: 'striker' },
+    'special': { group: 'class', value: 'special' },
+
+    'explosive': { group: 'attack', value: 'explosive' },
+    'red': { group: 'attack', value: 'explosive' },
+    'mystic': { group: 'attack', value: 'mystic' },
+    'blue': { group: 'attack', value: 'mystic' },
+    'penetration': { group: 'attack', value: 'penetration' },
+    'piercing': { group: 'attack', value: 'penetration' },
+    'yellow': { group: 'attack', value: 'penetration' },
+    'sonic': { group: 'attack', value: 'sonic' },
+    'purple': { group: 'attack', value: 'sonic' },
+
+    'light': { group: 'armor', value: 'light' },
+    'heavy': { group: 'armor', value: 'heavy' },
+    'specialarmor': { group: 'armor', value: 'special' },
+    'elastic': { group: 'armor', value: 'elastic' },
+
+    // Text search keywords
+    'ᓀ‸ᓂ': { text: '"azusa"' },
+    '`‸´': { text: '"kasumi"' }, //'-`‸´-': { text: 'kasumi' },
+    '눈': { text: '"fuuka"' }, //'눈_눈': { text: 'fuuka' },
+    '囧': { text: '"koyuki"' },
+    'sexy': { text: '"seia"' },
+};
+
+
+$(document).ready(function() {
+    $(".controls-search").append(
+        $('<input>', {
+            type: 'text',
+            id: 'charactertable-search',
+            placeholder: 'Filter...',
+        })
+    );
+    
     initCharacterTableFilters();
+    updateFiltersUI();
+    lastSearchSegments = $("#charactertable-search").val().toLowerCase().split(/[\s_,;+\-]+/).filter(seg => seg.length >= 2);
 });
 
-function initCharacterTableFilters() {
-    var filter = $("#charactertable-filter");
 
-    charactertable_filter.groups = {};
-    charactertable_filter.filters = {};
+function initCharacterTableFilters() {
+    const filter = $("#charactertable-filter");
+
+    quick_filter.groups = new Set();
+    quick_filter.filters = {};
 
     $.each(filter.find('div.controls > span'), function() {
-        var toggle = $(this).attr('data-toggle');
-        if (typeof(charactertable_filter.filters[toggle.split('-',1)[0]]) == 'undefined') charactertable_filter.filters[toggle.split('-',1)[0]] = {};
-
-        charactertable_filter.groups[toggle.split('-',1)[0]] = false;
-        charactertable_filter.filters[toggle.split('-',1)[0]][toggle] = false;
+        //var toggle = $(this).attr('data-toggle');
+        const [group, value] = $(this).attr('data-toggle').split('-');
+        //console.log('Quick filter button:', group, value);
+        if (typeof(group) !== 'undefined') {
+            quick_filter.groups.add(group);
+            if (!quick_filter.filters.hasOwnProperty(group)) quick_filter.filters[group] = new Set();
+            quick_filter.filters[group].add(value);
+        } 
     });
 
-    filter.find("div.controls > span").addClass('inactive').on("click", function(){characterTableToggle($(this));});
-}
 
-
-function updateCharacterTableFilterGroups () {
-    Object.keys(charactertable_filter.groups).forEach(group => {
-        //Filter isn't applied if ALL group elements are in the same state (either on or off) 
-        if (Object.values(charactertable_filter.filters[group]).every(element => element === true) 
-         || Object.values(charactertable_filter.filters[group]).every(element => element === false)) {
-            charactertable_filter.groups[group] = false;
-        }
-        else {
-            charactertable_filter.groups[group] = true;
-        }
-    });
-}
-
-
-function applyCharacterTableFilter () {
-
-    $(".charactertable tr").each(function (){
-        var hideRow = false;
-        Object.keys(charactertable_filter.groups).forEach(group => {
-            if (charactertable_filter.groups[group] === true) {
-                Object.keys(charactertable_filter.filters[group]).forEach(filter => {
-                    if (charactertable_filter.filters[group][filter] === false && $(this).hasClass(filter)) hideRow = true;
-                });
+    let initialFilters = [];
+    if (window.location.hash.length > 1) {
+        const uri = decodeURIComponent(window.location.hash.substring(1));
+        const segments = new Set(uri.split('_')); //.filter(seg => seg.length >= 2);
+        for (let seg of segments) {
+            seg = seg.replace(/^-+|-+$/g, '');
+            if (charactertable_search_keywords.hasOwnProperty(seg)) {
+                if (charactertable_search_keywords[seg].hasOwnProperty('text')) {
+                    initialFilters.push({type: 'text', src: 'textfield', value: charactertable_search_keywords[seg].text});
+                } else {
+                    const {group, value} = charactertable_search_keywords[seg];
+                    const is_quickfilter = quick_filter.groups.has(group);
+                    initialFilters.push({type: 'param', src: is_quickfilter?'quickfilter':'textfield', group, value});
+                }
+            } else if (seg.includes('-')) {
+                const [group, value] = seg.split('-');
+                const is_quickfilter = quick_filter.groups.has(group);
+                initialFilters.push({type: 'param', src: is_quickfilter?'quickfilter':'textfield', group, value});
+            } else {
+                initialFilters.push({type: 'text', src:'textfield', value: seg});
             }
-        });
-        
-        if (hideRow) $(this).addClass('hidden').removeClass('visible');
-        else $(this).addClass('visible').removeClass('hidden');
+        }
+    }
+    charactertable_filters = initialFilters;
+
+
+    // Event handlers
+    filter.find("div.controls > span").addClass('inactive').on("click", function() { characterTableToggle($(this)); });
+    $("#charactertable-search").on("input", function() {characterTableTextFilter($(this).val()); });
+
+
+    // Sync button states
+    $("#charactertable-filter div.controls > span").each(function() {
+        const toggle = $(this).attr('data-toggle');
+        const [group, value] = toggle.split('-');
+        const active = charactertable_filters.some(f => f.type === 'param' && f.group === group && f.value === value);
+        $(this).toggleClass('active', active).toggleClass('inactive', !active);
     });
 }
 
 
-function characterTableToggle (toggleItem = false) {
-	//console.log('toggling '+toggleItem.attr('data-toggle'));
-	//toggleItem = (typeof toggleItem !== 'undefined') ? toggleItem : false; //default false, ES5 does not support function defaults
+function characterTableToggle(toggleItem) {
+    const toggle = $(toggleItem).attr('data-toggle');
+    const [group, value] = toggle.split('-');
 
-	if (toggleItem) {
-        var toggle = $(toggleItem).attr('data-toggle');
+    // Remove any existing filter for this group/value
+    charactertable_filters = charactertable_filters.filter(f => !((f.type === 'param' || f.type === 'keyword') && f.src === 'quickfilter' && f.group === group && f.value === value));
+    if (toggleItem.hasClass("inactive")) {
+        //console.log('Adding filter', {type: 'param', group, value});
+        toggleItem.addClass('active').removeClass('inactive');
+        charactertable_filters.push({type: 'param', src: 'quickfilter', group, value});
+    } else {
+        //console.log('Removing filter', {type: 'param', group, value});
+        toggleItem.addClass('inactive').removeClass('active');
+        // Already removed above
+    }
+    updateFiltersUI();
+}
 
-		if (toggleItem.hasClass("inactive")) {
-			toggleItem.addClass('active').removeClass('inactive');
-            charactertable_filter.filters[toggle.split('-',1)[0]][toggle] = true;
-            //console.log(`Toggled ON filter ${toggle.split('-',1)[0]}.${toggle}`);
-		}
-		else {
-			toggleItem.addClass('inactive').removeClass('active');
-            charactertable_filter.filters[toggle.split('-',1)[0]][toggle] = false;
-            //console.log(`Toggled OFF filter ${toggle.split('-',1)[0]}.${toggle}`);
-		}
 
-        updateCharacterTableFilterGroups();
-        applyCharacterTableFilter();
-	}
+function characterTableTextFilter(searchStr) {
+    const segments = searchStr.toLowerCase().split(/[\s_,;+\-]+/); //.filter(seg => seg.length >= 2);
+    // Only update if segments changed
+    if (segments.join('|') === lastSearchSegments.join('|')) {
+        //console.log('Offramp: No change in search segments \n' + segments.join('|') + '\n' + lastSearchSegments.join('|'));
+        return;
+    }
+    lastSearchSegments = segments;
+
+    // Remove all previous textfield filters from array, readd new ones
+    charactertable_filters = charactertable_filters.filter(f => f.src !== 'textfield');
+
+    for (const seg of segments) {
+        if (charactertable_search_keywords.hasOwnProperty(seg)) {
+            //console.log('Keyword match:', seg, charactertable_search_keywords[seg]);
+            if (charactertable_search_keywords[seg].hasOwnProperty('text')) {
+                //console.log('Adding text filter:', charactertable_search_keywords[seg].text);
+                charactertable_filters.push({type: 'text', src: 'textfield', value: charactertable_search_keywords[seg].text});
+            } else {
+                //console.log('Adding param filter:', charactertable_search_keywords[seg]);
+                const {group, value} = charactertable_search_keywords[seg];
+                const is_quickfilter = quick_filter.groups.has(group);
+                charactertable_filters.push({type: 'param', src: is_quickfilter?'quickfilter':'textfield', group, value});
+            }
+        } else if (seg.length > 2 && seg.includes(':')) {
+            const [group, value] = seg.split(':');
+            const is_quickfilter = quick_filter.groups.has(group);
+            if (quick_filter.groups.has(group) && quick_filter.filters[group].has(value)) {
+                //console.log('Adding quickfilter param filter:', {group, value});
+                charactertable_filters.push({type: 'param', src: is_quickfilter?'quickfilter':'textfield', group, value});
+            } else if (quick_filter.groups.has(group)) {
+                // Do nothing, wait for valid quickfilter value for this group
+            }
+            else {
+                //console.log('Adding textfield param filter:', {group, value});
+                charactertable_filters.push({type: 'param', src: 'textfield', group, value});
+            }
+        } else if (seg.length > 2) {
+            charactertable_filters.push({type: 'text', src: 'textfield', value: seg});
+        }
+    }
+
+    
+    // Sync button states
+    $("#charactertable-filter div.controls > span").each(function() {
+        const toggle = $(this).attr('data-toggle');
+        const [group, value] = toggle.split('-');
+        const active = charactertable_filters.some(f => f.src === 'quickfilter' && f.group === group && f.value === value);
+        $(this).toggleClass('active', active).toggleClass('inactive', !active);
+    });
+
+    updateFiltersUI();
+}
+
+
+// Update UI and filtering
+function updateFiltersUI() {
+    // Update text field (only param/text filters) IF not focused
+    const $search = $("#charactertable-search");
+    if (!$search.is(":focus")) {
+        $search.val(filtersToTextField(charactertable_filters));
+    }
+
+    // Apply filtering to table
+    $(".charactertable tr").each(function () {
+        if ($(this).closest('thead').length) return;
+        let showRow = true;
+        const nameCell = $(this).find("td").eq(1);
+
+        // Collect all filters by group
+        const filteredGroups = {};
+        for (const f of charactertable_filters) {
+            if (f.type === 'param' || f.type === 'keyword') {
+                if (!filteredGroups[f.group]) filteredGroups[f.group] = new Set();
+                filteredGroups[f.group].add(f.value);
+            }
+        }
+        
+        // Check param filters (additive within group)
+        for (const group in filteredGroups) {
+            if (!matchConditional(this, group, filteredGroups[group])) {
+                showRow = false;
+                break;
+            }
+        }
+
+        // Check text filters
+        if (showRow) {
+            for (const f of charactertable_filters) {
+                if (f.type === 'text') {
+                    if (!nameCell.length) {
+                        showRow = false;
+                        break;
+                    }
+                    const cellText = nameCell.text().toLowerCase();
+                    if (f.value.startsWith('"') && f.value.endsWith('"')) {
+                        // Whole-word match for quoted text
+                        const quoted = f.value.slice(1, -1).toLowerCase();
+                        // Use word boundaries for whole word match
+                        const regex = new RegExp(`\\b${quoted}\\b`, 'i');
+                        if (!regex.test(cellText)) {
+                            showRow = false;
+                            break;
+                        }
+                    } else {
+                        // Partial match for normal text
+                        if (cellText.indexOf(f.value) === -1) {
+                            showRow = false;
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        $(this).toggleClass('visible', showRow).toggleClass('hidden', !showRow);
+    });
+
+    // Update URL anchor
+    window.location.hash = encodeURIComponent(filtersToURI(charactertable_filters));
+}
+
+
+function matchConditional(row, key, values) {
+    // Support values as a Set, Array, or single string
+    let valueList = Array.isArray(values) ? values : (values instanceof Set ? Array.from(values) : [values]);
+
+    const rowVal = $(row).data(key);
+    if (rowVal === undefined) return true;
+
+    // Check each value in the list
+    for (let val of valueList) {
+        let op = null, cmpVal = val;
+        if (typeof val === 'string') {
+            if (val.startsWith('>=')) { op = '>='; cmpVal = val.slice(2); }
+            else if (val.startsWith('<=')) { op = '<='; cmpVal = val.slice(2); }
+            else if (val.startsWith('>')) { op = '>'; cmpVal = val.slice(1); }
+            else if (val.startsWith('<')) { op = '<'; cmpVal = val.slice(1); }
+        }
+
+        // Numeric compare
+        if (op && !isNaN(cmpVal) && !isNaN(rowVal)) {
+            const nRow = Number(rowVal), nVal = Number(cmpVal);
+            if (op === '>' && nRow > nVal) return true;
+            if (op === '>=' && nRow >= nVal) return true;
+            if (op === '<' && nRow < nVal) return true;
+            if (op === '<=' && nRow <= nVal) return true;
+            if (!op && nRow === nVal) return true;
+        }
+        // Affinity compare (urban, outdoors, indoors)
+        else if (['urban','outdoors','indoors'].includes(key) && op) {
+            if (affinityCompare(rowVal, op, cmpVal)) return true;
+        }
+        // Default: string match
+        else if (String(rowVal).toLowerCase() === String(cmpVal).toLowerCase()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// Helper for affinity letter order
+const affinityOrder = ['d','c','b','a','s','ss'];
+function affinityCompare(rowVal, op, val) {
+    const rowIdx = affinityOrder.indexOf(rowVal.toLowerCase());
+    const valIdx = affinityOrder.indexOf(val.toLowerCase());
+    if (rowIdx === -1 || valIdx === -1) return false;
+    if (op === '>') return rowIdx > valIdx;
+    if (op === '>=') return rowIdx >= valIdx;
+    if (op === '<') return rowIdx < valIdx;z
+    if (op === '<=') return rowIdx <= valIdx;
+    return rowIdx === valIdx;
+}
+
+// Utility: convert filter array to URI string
+function filtersToURI(filters) {
+    return filters.map(f => {
+        if (f.type === 'keyword') return f.value;
+        if (f.type === 'param') return `${f.group}-${f.value}`;
+        if (f.type === 'text') return f.value;
+    }).join('_');
+}
+
+// Utility: convert filter array to text field string (only param/text filters)
+function filtersToTextField(filters) {
+    //console.log(filters);
+    return filters.filter(f => f.src === 'textfield').map(f => {
+        if (f.type === 'keyword') return f.value;
+        if (f.type === 'param') return `${f.group}:${f.value}`;
+        if (f.type === 'text') return f.value;
+    }).join(' ');
+}
+
+// Utility: parse text field into param/text filters
+function parseTextField(str) {
+    const segments = str.toLowerCase().split(/[\s_,;+]+/).filter(seg => seg.length >= 2);
+    return segments
+        .filter(seg => {
+            // Only allow text search segments with length >= 3, or param segments
+            if (seg.includes(':')) return true;
+            return seg.length >= 3;
+        })
+        .map(seg => {
+            if (seg.includes(':')) {
+                const [group, value] = seg.split(':');
+                return {type: 'param', group, value};
+            }
+            return {type: 'text', value: seg};
+        });
 }
